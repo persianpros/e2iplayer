26f09346852436224f65d41604ed4fb2ce9246e7
Hains van den Bosch <hainsvdbosch@ziggo.nl>
2019-12-22 18:05:04 +0100

    iptvsetupimpl.py: Get rid of version numbers
    They were used for the checks(which are removed now) and our OE core
    is always on the move.

--- IPTVPlayer/setup/iptvsetupimpl.py
+++ IPTVPlayer/setup/iptvsetupimpl.py
@@ -25,64 +25,51 @@ class IPTVSetupImpl:
         Screen.__init__(self, session)
 
         self.tmpDir = GetTmpDir()
-        
+
         self.ffmpegVersion = ""
         self.gstreamerVersion = ""
         self.openSSLVersion = ""
         self.libSSLPath = ""
         self.platform = "unknown"
         self.glibcVersion = -1
-        
+
         # wget members
-        self.wgetVersion = 1902 # 1.15 
         self.wgetpaths = ["wget", "/usr/bin/wget"]
 
         # rtmpdump members
-        self.rtmpdumpVersion = 20151215 #{'sh4':'2015', 'mipsel':'2015', 'armv5t':'2015', 'armv7':'2015', 'default':"Compiled by samsamsam@o2.pl 2015-01-11"} #'K-S-V patch'
         self.rtmpdumppaths = ["/usr/bin/rtmpdump", "rtmpdump"]
         
         # f4mdump member
-        self.f4mdumpVersion = 0.80
         self.f4mdumppaths = ["/usr/bin/f4mdump", GetBinDir("f4mdump", "")]
                                           
         # uchardet member
-        self.uchardetVersion = [0, 0, 6] #UCHARDET_VERSION_MAJOR, UCHARDET_VERSION_MINOR, UCHARDET_VERSION_REVISION
         self.uchardetpaths = ["/usr/bin/uchardet", GetBinDir("uchardet", "")]
 
         # gstplayer
-        self.gstplayerVersion = {'0.10':20, '1.0':10021}
         self.gstplayerpaths = ["/usr/bin/gstplayer", GetBinDir("gstplayer", "")]
 
         # exteplayer3
-        self.exteplayer3Version = {'sh4':50, 'mipsel':50, 'armv7':50, 'armv5t':50}
         self.exteplayer3paths = ["/usr/bin/exteplayer3", GetBinDir("exteplayer3", "")]
                                           
         # flumpegdemux
-        self.flumpegdemuxVersion = "0.10.85"
         self.flumpegdemuxpaths = ["/usr/lib/gstreamer-0.10/libgstflumpegdemux.so"]
         
         # gstifdsrc
-        self.gstifdsrcVersion = "1.1.1"
         self.gstifdsrcPaths = ["/usr/lib/gstreamer-1.0/libgstifdsrc.so"]
         
         # subparser
-        self.subparserVersion = 0.5
         self.subparserPaths = [resolveFilename(SCOPE_PLUGINS, 'Extensions/IPTVPlayer/libs/iptvsubparser/_subparser.so')]
 
         # e2icjson
-        self.e2icjsonVersion = 10202 #'1.2.2' int(z[0]) * 10000 + int(z[1]) * 100 + int(z[2])
         self.e2icjsonPaths = [resolveFilename(SCOPE_PLUGINS, 'Extensions/IPTVPlayer/libs/e2icjson/e2icjson.so')]
 
         # hlsdl
-        self.hlsdlVersion = 0.21
         self.hlsdlPaths = ["/usr/bin/hlsdl"]
         
         # cmdwrap
-        self.cmdwrapVersion = 2
         self.cmdwrapPaths = ["/usr/bin/cmdwrapper"]
         
         # duk
-        self.dukVersion = 6 # "2.1.99 [experimental]" # real version
         self.dukPaths = ["/usr/bin/duk"]
         
         self.tries = 0
@@ -90,4 +77,3 @@ class IPTVSetupImpl:
         self.abiFP = None
         
         self.finish()
- 
